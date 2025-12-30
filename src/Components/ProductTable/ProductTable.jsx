50786f68eba8a1df54f69e85d184402bab417449
Abhranil Kundu <abhranilkundu06@gmail.com>
2025-12-30 20:58:22 +0530

    commit

--- src/Components/ProductTable/ProductTable.jsx
+++ src/Components/ProductTable/ProductTable.jsx
@@ -11,7 +11,7 @@ import { Bounce, toast, ToastContainer } from "react-toastify";
 import { faEye, faPencil, faTrash } from "@fortawesome/free-solid-svg-icons";
 import TableSkeleton from "../Skeleton/TableSkeleton/TableSkeleton.jsx";
 import Popup from "../Popup/Popup.jsx";
-import LoaderBox from "../loaderBox/LoaderBox.jsx";
+import LoaderBox from "../LoaderBox/LoaderBox.jsx";
 import ProductViewCard from "../ProductViewCard/ProductViewCard.jsx";
 
 function ProductTable({ data, error, isLoading }) {

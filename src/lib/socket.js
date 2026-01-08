8d4ea7a11a086f2fbe963c235028f04868da2121
Abhranil Kundu <abhranilkundu06@gmail.com>
2026-01-08 21:09:47 +0530

    commit

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("https://www.dsportdb.online", {
+export const socket = io("https://www.dsportdb.online/", {
   withCredentials: true,
   transports: ["polling", "websocket"], // 👈 DO NOT FORCE websocket
   autoConnect: true,

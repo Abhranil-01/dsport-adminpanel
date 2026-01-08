cb809b7c7c8f6b22c6577d470ac22f676e30c1e7
Abhranil Kundu <abhranilkundu06@gmail.com>
2026-01-08 17:37:20 +0530

    commit

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -3,4 +3,5 @@ import { io } from "socket.io-client";
 export const socket = io(import.meta.env.VITE_PUBLIC_SOCKET_URL, {
   withCredentials: true,
   autoConnect: true,
+  transports: ["polling", "websocket"], // needed for Vercel
 });
